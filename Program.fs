9f12a7ee389ec1450d0ff6144389939630b2a512
Magnus Ulf <magnus@muj.dk>
2022-02-21 11:12:17 +0100

    First version of ValueIteration

---
+++ Program.fs
@@ -0,0 +1,109 @@
+﻿open System
+open ValueIteration
+
+// For more information see https://aka.ms/fsharp-console-apps
+type State = int * int
+type Action = 
+    | Up = 0
+    | Down = 1
+    | Left = 2
+    | Right = 3
+type Reward = float
+type BaseTransitionFunction = State->Action->((State * float) list)
+type TransitionFunction = State->Action->State->float
+type RewardFunction = State->Action->State->Reward
+type MDP = { X : State list; A : Action list; p : TransitionFunction; r : RewardFunction }
+
+let absorbtionMarker: State = (-1, -1)
+
+
+let convertTransitionFunction (bt: BaseTransitionFunction) : TransitionFunction =
+    let tf (s1: State) (a: Action) (s2: State) : float =
+        let probs = bt s1 a
+        let filtered = List.filter (fun x -> fst x = s2) probs
+        match filtered with
+        | n :: rest -> snd n
+        | [] -> 0
+    tf
+
+let stateToString (s: State) : string =
+    $"({fst s}, {snd s})"
+
+let actionToString (a: Action) : string =
+    match a with
+    | Action.Up -> "Up"
+    | Action.Down -> "Down"
+    | Action.Left -> "Left"
+    | Action.Right -> "Right"
+    | _ -> ""
+
+let rec getRandomElementWeighted (lst: 'a list) (weigths: float list) (r: float): 'a =
+    if (r - (List.head weigths)) > 0 then
+        getRandomElementWeighted (List.tail lst) (List.tail weigths) (r - List.head weigths)
+    else
+        List.head lst
+
+let maximumX = 2
+let maximumY = 2
+
+let applyActionDirectly (stateFrom: State) (action: Action) : State =
+    let keepWithinBorders (s: State) : State =
+        max 0 (min maximumX (fst s)), max 0 (min maximumY (snd s))
+
+    let step1 = match action with
+                | Action.Up -> fst stateFrom, snd stateFrom + 1
+                | Action.Down -> fst stateFrom, snd stateFrom - 1
+                | Action.Left -> fst stateFrom - 1, snd stateFrom
+                | Action.Right -> fst stateFrom + 1, snd stateFrom 
+                | _ -> raise (ArgumentException("Error"))
+        
+    let step2 = keepWithinBorders step1
+    step2
+
+let turnRight (action: Action) : Action =
+    match action with
+    | Action.Up -> Action.Right
+    | Action.Down -> Action.Left
+    | Action.Left -> Action.Up
+    | Action.Right -> Action.Down
+    | _ -> raise (System.ArgumentException("Error"))
+
+let turnLeft (action: Action) : Action =
+    turnRight (turnRight (turnRight action))
+
+let btf1 (stateFrom: State) (action: Action) : (State * float) list =
+    if (stateFrom = absorbtionMarker) then
+        [absorbtionMarker, 1]
+    else if (stateFrom = (0,0) || stateFrom = (0,2)) then
+        [absorbtionMarker, 1]
+    else
+        [(applyActionDirectly stateFrom action), 0.8 ; (applyActionDirectly stateFrom (turnRight action)), 0.1 ; (applyActionDirectly stateFrom (turnLeft action)), 0.1]
+
+let tf1: TransitionFunction = convertTransitionFunction btf1
+let rf1 sFrom a sTo: float =
+    if (sFrom = (0,0) && sTo=absorbtionMarker) then
+        1
+    else if (sFrom = (0,2) && sTo=absorbtionMarker) then
+        -0.5
+    else
+        0
+
+let si (s: State): int =
+    if (s = absorbtionMarker) then
+        9
+    else
+        (fst s)*3+(snd s)
+
+let mdp_x = [(0,0); (0,1); (0,2); (1,0); (1,1); (1,2); (2,0); (2,1); (2,2) ; absorbtionMarker]
+let mdp_a = [Action.Up ; Action.Down ; Action.Left ; Action.Right]
+
+let mdp = {X = mdp_x ; A = mdp_a ; p = tf1 ; r = rf1 ;
+si = si ; ai = int }
+
+printfn "Hello from F#"
+
+let Q = ValueIteration.valueIteration mdp 0.9
+
+for x in mdp.X do
+    //for a in mdp.A do
+    printfn "%s: %.3f" (stateToString x) (Array.max Q[si x, *])
---
+++ ValueIteration.fs
@@ -0,0 +1,62 @@
+module ValueIteration
+
+type StateIdx<'s> = 's->int
+type ActionIdx<'a> = 'a->int
+
+type Reward = float
+type TransitionFunction<'s, 'a> = 's->'a->'s->float
+type RewardFunction<'s, 'a> = 's->'a->'s->Reward
+type MDP<'s, 'a> = { X : 's list; A : 'a list; p : TransitionFunction<'s, 'a>; r : RewardFunction<'s, 'a> ; si: StateIdx<'s> ; ai: ActionIdx<'a> }
+
+/// Calculates a matrix that stores the transition probabilities
+/// We store it so it needs not be calculated often
+let getTransitionMatrix(mdp: MDP<'s, 'a>) : float[,,] =
+    let ret: float[,,] = Array3D.init (List.length mdp.X) (List.length mdp.A) (List.length mdp.X) (fun i j k -> 0.0)
+    for s1 in mdp.X do
+        for a in mdp.A do
+            for s2 in mdp.X do
+                ret[mdp.si s1, mdp.ai a, mdp.si s2] <- mdp.p s1 a s2
+    ret
+
+/// Calculates a matrix that stores the rewards
+/// We store it so it needs not be calculated often
+let getRewardMatrix(mdp: MDP<'s, 'a>) : float[,,] =
+    let ret: float[,,] = Array3D.init (List.length mdp.X) (List.length mdp.A) (List.length mdp.X) (fun i j k -> 0.0)
+    for s1 in mdp.X do
+        for a in mdp.A do
+            for s2 in mdp.X do
+                ret[mdp.si s1, mdp.ai a, mdp.si s2] <- mdp.r s1 a s2
+    ret
+
+/// Given a state and the calculated Q-table get the value of the state
+/// Absorbtion states are those where 'si s < 0'
+let getStateValue<'s> (si: StateIdx<'s>) (s: 's) (Q: float[,]) : float =
+    Array.max Q[si s, *]
+
+/// Given a starting state an action and the end state what is the value
+/// This is equal to the transition reward plus the discounted (expected) value of the end state
+let getActionToStateValue (discount: float) (si: StateIdx<'s>) (ai: ActionIdx<'a>) (stateFrom: 's) (action: 'a) (stateTo: 's) (R: float[,,])  (Q: float[,]) : float =
+    let transitionReward = R[si stateFrom, ai action, si stateTo]
+    let nextStateValue = getStateValue si stateTo Q
+    transitionReward + discount * nextStateValue
+
+/// Given a starting state and an action it gives the discounted, expected value
+/// Of performing the action
+let getActionValue (discount: float) (mdp: MDP<'s, 'a>) (stateFrom: 's) (action: 'a) (P: float[,,]) (R: float[,,])  (Q: float[,]) : float =
+    let prob stateTo = P[mdp.si stateFrom, mdp.ai action, mdp.si stateTo]
+    let value stateTo = getActionToStateValue discount mdp.si mdp.ai stateFrom action stateTo R Q
+    let probValue stateTo = (prob stateTo) * (value stateTo)
+    List.sum (List.map probValue mdp.X)
+
+let valueIteration<'s, 'a> (mdp: MDP<'s, 'a>) (discount: float): float[,] =
+    let P = getTransitionMatrix mdp
+    let R = getRewardMatrix mdp
+    
+    let Q: float[,] = Array2D.init (List.length mdp.X) (List.length mdp.A) (fun i j -> 0.0)
+
+    for i in 1 .. 10 do
+        for x in mdp.X do
+                for a in mdp.A do
+                    Q[mdp.si x, mdp.ai a] <- getActionValue discount mdp x a P R Q
+
+    Q
